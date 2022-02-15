9f6a17c25e64668a55b531289f62d1fb0810934e
Ajayi Olamide Daniel <prodbyola@gmail.com>
2022-02-15 07:55:02 +0100

    Still trying to commit.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use('/auth', AuthRouter)
 app.use('/video', VRouter)
 
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
     console.log('Listening on port', port)
 
     const dbConnect = new AppDB()
